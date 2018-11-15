6d270f8b6a82cf8200ae6c501c9ddd323b17cd34
André Morais <andre.morais@blip.pt>
2018-11-15 14:55:19 +0000

    Fix config

--- config.py
+++ config.py
@@ -4,17 +4,12 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
-db_password = ''
-
-if os.environ.get('DATABASE_PASSWORD_FILE'):
-    with open('/run/secrets/db_twipy_password') as f:
-        db_password = f.read().strip()
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db')).format(
-        user=os.environ.get('DATABASE_USER'), password=db_password,
+        user=os.environ.get('DATABASE_USER'), password=os.environ.get('DATABASE_PASSWORD'),
         database=os.environ.get('DATABASE'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 8
@@ -22,7 +17,7 @@ class Config(object):
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', None)
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME', None)
-    MAIL_PASSWORD = os.environ.get('MAIL_USERNAME', None)
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', None)
     ADMINS = ['admin@twipy.com']
     FLAG = os.environ.get('FLAG')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
